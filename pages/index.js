39de413934e16190eda9e39c82c212fc713f7c13
Miguel <miguelnietolopez2002@gmail.com>
2022-04-03 14:50:59 -0500

    setup next.js layout, set up basic pages js files, start on header in pages/index.js

---
+++ pages/index.js
@@ -0,0 +1,28 @@
+const headerHtml = 
+<div id="header">
+    <ul id="navbar">
+        <li class="navItem" id="homeLink"><a>Home</a></li>
+        <li class="navItem" id="solutionsLink"><a>Solutions</a></li>
+        <li class="navItem" id="aboutLink"><a>About Us</a></li>
+    </ul>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function HomePage() {
+    return <div>Welcome to Next.Js!</div>
+}
+
+export default HomePage
