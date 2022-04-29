92e0fa7d0a48d0880dd36b91edd3feceb950f834
Miguel <miguelnietolopez2002@gmail.com>
2022-04-28 21:48:02 -0500

    added css items and classes to header.js

--- common/header.js
+++ common/header.js
@@ -2,8 +2,11 @@ export default function Header() {
     return (
         <div id="header">
         <ul>
-           <li><a href="/">Home</a></li>
-           <li><a href="/about">About</a></li>
+            <li><img id="headerImage" src=""></img></li>
+           <li><a id="homeItem" class="item" href="/">Home</a></li>
+           <li><a id="aboutItem" class="item" href="/about">About</a></li>
+           <li><a id="solutionsItem" class="item" href="/solutions">Solutions</a></li>
+           <li><a id="contactItem" class="item" href="/contact">Contact Us</a></li>
        </ul> 
         </div>
     )
