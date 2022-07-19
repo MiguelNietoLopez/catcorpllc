f3a36204232dd980572606ee420832c486f66ce7
Miguel <miguelnietolopez2002@gmail.com>
2022-07-19 01:19:47 -0500

    See last Commit

--- pages/contact.js
+++ pages/contact.js
@@ -32,11 +32,28 @@ class ContactApp extends React.Component{
     submitForm(e)
     {
         e.preventDefault();
+        let first = this.verifyName(this.state.firstName);
+        let last = this.verifyName(this.state.lastName);
+        let email = this.verifyEmail(this.state.email);
+        let message = this.verifyMessage(this.state.message);
+        switch (false)
+        {
+            case first:
+                console.log('firstname invalid');
+            case last:
+                console.log('lastname invalid');
+            case email:
+                console.log('email invalid');
+            case message:
+                console.log('message not valid');
+            default:
+                break;
+        }
         let body = {
-            firstName: this.state.firstName,
-            lastName: this.state.lastName,
-            email: this.state.email,
-            message: this.state.message
+            firstName: first,
+            lastName: last,
+            email: email,
+            message: message
         }
         let option = {
             mode: 'cors',
@@ -45,6 +62,42 @@ class ContactApp extends React.Component{
             body: JSON.stringify(body)
         };
         fetch('http://localhost:3000/api/contact_submit', option).then(() => { console.log('sent')});
+    }
+    verifyName(name){
+        let reg =/^[\p{L}]+$/gu;
+        if (reg.test(name) == true)
+        {
+            return name;
+        }
+        else 
+        {
+            return false;
+        }
+    }
+    verifyEmail(email){
+        let reg = /^\w+@\w+.(com|org|edu|net|co)/gi;
+        if (reg.test(email) == true)
+        {
+            return email;
+        }
+        else
+        {
+            return false;
+        }
+
+
+    }
+    verifyMessage(message){
+        let reg = /^([\p{L}]|\.|\s)+/gu;
+        //let reg =/^[\p{L}]+$/gu;
+        if (reg.test(message) == true)
+        {
+            return message;
+        }
+        else
+        {
+            return false;
+        }
     }
 
     render(){
--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,16 @@ class IndexApp extends React.Component {
         return(
             <div id="indexApp">
                 {Header()}
+                <h1 id="title">Cat Corp LLC</h1>
+                <h2 id="subtitle">Number One Cat based Software Firm</h2>
+                <img src="common/images/business-cat-in-office.jpg" alt="Cat Corp LLC banner image"></img>
+                <p id="main">We provide many different services and can help you.</p>
+                <p id="stats">Our customers see an average of:</p>
+                <img src="" alt="customer service"></img>
+                <p id="stats2">5000x more purchases, 90000x more return customers, and an average life expectancy of 35000 years</p>
+                <img src="/common/images/13122-01-data-driven-infographic-16x9-1.jpg" alt="good chart"></img>
+                <img src="/common/images/th-2380061417.jpg" alt="chart 2"></img>
+                <img src="/common/images/srdffd.PNG" alt="thumbsup"></img>
                 {Footer()}
             </div>
         )
