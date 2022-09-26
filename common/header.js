4a271eb74f3a34ee8726c0444cbeabd328aaee12
Miguel <miguelnietolopez2002@gmail.com>
2022-09-26 15:50:36 -0500

    header has basic shape and layout

--- common/header.js
+++ common/header.js
@@ -1,13 +1,10 @@
 export default function Header() {
     return (
         <div id="header">
-        <ul>
-            <li><img id="headerImage" src=""></img></li>
-           <li><a id="homeItem" className="item" href="/">Home</a></li>
-           <li><a id="aboutItem" className="item" href="/about">About</a></li>
-           <li><a id="solutionsItem" className="item" href="/solutions">Solutions</a></li>
-           <li><a id="contactItem" className="item" href="/contact">Contact Us</a></li>
-       </ul> 
+            <a id="homeItem" className="item" href="/">Home</a>
+            <a id="aboutItem" className="item" href="/about">About</a>
+            <a id="solutionsItem" className="item" href="/solutions">Solutions</a>
+            <a id="contactItem" className="item" href="/contact">Contact</a>
         </div>
     )
 };
