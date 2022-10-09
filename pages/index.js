1467605d0cdd115b1c95d4a7cb2b437501a4c0a2
Miguel <miguelnietolopez2002@gmail.com>
2022-10-08 20:14:11 -0500

    made changes to css, improved text responsiveness

--- pages/index.js
+++ pages/index.js
@@ -13,12 +13,11 @@ class IndexApp extends React.Component {
                     <h1 id="title">Cat Corp LLC</h1>
                     <h2 id="subtitle">Number One Cat based Software Firm</h2>
                     <img id="img1" className="img"  src="common/images/business-cat-in-office.jpg" alt="Cat Corp LLC banner image"></img>
-                    <p className="centerText" id="main">We provide many different services and can help you.</p>
-                    <p className="centerText" id="stats">Our customers see an average of:</p>
+                    <p className="centerText imgDesc" id="main">We provide many different services and can help you.</p>
+                    <p className="centerText imgTitle" id="stats">Our customers see an average of:</p>
                     <img id="img2" className="img"  src="/common/images/13122-01-data-driven-infographic-16x9-1.jpg" alt="good chart"></img>
-                    <p className="centerText" id="stats2">5000x more purchases, 90000x more return customers, and an average life expectancy of 35000 years</p>
-                    <img id="img3" className="img"  src="/common/images/th-2380061417.jpg" alt="chart 2"></img>
-                    <p className="centerText" id="thumbsuptext">Contact Us Today for a Free quote</p>
+                    <p className="centerText imgDesc" id="stats2">5000x more purchases, 90000x more return customers, and an average life expectancy of 35000 years</p>
+                    <p className="centerText imgTitle" id="thumbsuptext">Contact Us Today for a Free quote</p>
                     <img id="img4" className="img" src="/common/images/srdffd.PNG" alt="thumbsup"></img>
                 </div>
                 {Footer()}
