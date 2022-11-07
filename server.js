2253e71598d1c24387d59f943ba49805f5c5d8e9
Miguel <miguelnietolopez2002@gmail.com>
2022-11-06 21:18:26 -0600

    small tweaks to package.json and server.js

--- server.js
+++ server.js
@@ -64,6 +64,5 @@ app.get(/.pages/, (req, res) => {
 var server = app.listen(3000, () => {
     var host = server.address().address;
     var port = server.address().port;
-
     console.log('test');
 })
