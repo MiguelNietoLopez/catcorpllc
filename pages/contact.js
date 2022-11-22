059b895450c3023a5ab3f08cc9fb17fff335bd7d
Miguel <miguelnietolopez2002@gmail.com>
2022-11-22 05:05:02 -0600

    added additional formatting changes and accesability changes, started working on contact.js final parts

--- pages/contact.js
+++ pages/contact.js
@@ -5,13 +5,22 @@ class ContactApp extends React.Component{
             firstName: '',
             lastName: '',
             email: '',
-            message: ''
+            message: '',
+            warningText: '',
+            warningArray: {
+                0 : '',
+                1 : 'Message Too Long Please Keep Under 200 Characters',
+                2 : 'Alphanumerical Characters(a-z 0-9) only please'
+            },
+            activeWarnings: new Array()
         };
         this.firstNameHandler = this.firstNameHandler.bind(this);
         this.lastNameHandler = this.lastNameHandler.bind(this);
         this.emailHandler = this.emailHandler.bind(this);
         this.messageHandler = this.messageHandler.bind(this);
         this.submitForm = this.submitForm.bind(this);
+        this.triggerWarning = this.triggerWarning.bind(this);
+        this.updateWarnings = this.updateWarnings.bind(this);
     };
     firstNameHandler(e)
     {
@@ -27,7 +36,44 @@ class ContactApp extends React.Component{
     };
     messageHandler(e)
     {
-        this.setState({message: e.target.value});
+        console.log(this.state.activeWarnings)
+        if (e.target.value.length <= 200){
+            this.triggerWarning(-1)
+            this.setState({message: e.target.value});
+        }
+        else if (!this.state.activeWarnings.includes(1)){
+            this.triggerWarning(1);
+            
+        }
+    }
+    updateWarnings()
+    {
+        let result = '';
+        console.log(this.state.activeWarnings)
+       this.state.activeWarnings.forEach(code => result += this.state.warningArray[code] + '\n');
+       this.setState({warningText: result});
+    }
+    triggerWarning(code)
+    {
+        console.log(typeof this.state.activeWarnings)
+        let temp = this.state.activeWarnings; 
+        switch (code)
+        {
+            case -2:
+                this.setState({activeWarnings: [].concat(temp.filter(x => x == 1))},() => this.updateWarnings());
+                break;
+            case -1:
+                console.log('jt')
+                this.setState({activeWarnings: [].concat(temp.filter(x => x == 2))},() => this.updateWarnings());
+                break;
+            case 1:
+                this.setState({activeWarnings: [].concat([1])},() => this.updateWarnings());
+                break;
+            case 2:
+                this.setState({activeWarnings: [].concat([2])},() => this.updateWarnings());
+                break;
+        }
+        
     }
     submitForm(e)
     {
@@ -106,17 +152,28 @@ class ContactApp extends React.Component{
                 {Header()}
                 <h1 id="title">Contact Us</h1>
                 <h3 id="subtitle">Fill out this form and we will get in touch with you</h3>
-                <form id="contactForm">
-                    <label id="firstNameLabel" className="label" htmlFor="firstName">First Name</label>
-                    <input type="text" name="firstName" onInput={this.firstNameHandler}></input>
-                    <label id="lastNameLabel" className="label" htmlFor="lastName">Last Name</label>
-                    <input type="text" name="lastName" onInput={this.lastNameHandler}></input>
-                    <label id="emailLabel" htmlFor="email" >Email</label>
-                    <input types="email" name="email" onInput={this.emailHandler}></input>
-                    <label id="messageLabel" className="label" htmlFor="message">Message</label>
-                    <textarea id="message" name="message" rows="4" onInput={this.messageHandler}></textarea>
-                    <button id="formButton" onClick={this.submitForm}>Submit</button>
-                </form>
+                <div id="centerContent">
+                    <form id="contactForm">
+                        <div id="firstName" className="formDiv">
+                            <label id="firstNameLabel" className="label" htmlFor="firstName">First Name </label>
+                            <input type="text" className="inputs" name="firstName" onInput={this.firstNameHandler}></input>
+                        </div>
+                        <div id="lastName" className="formDiv">
+                            <label id="lastNameLabel" className="label" htmlFor="lastName">Last Name </label>
+                            <input type="text" name="lastName"  className="inputs" onInput={this.lastNameHandler}></input>
+                        </div>
+                        <div id="email" className="formDiv">
+                            <label id="emailLabel" htmlFor="email" >Email </label>
+                            <input className="inputs" types="email" name="email" onInput={this.emailHandler}></input>
+                        </div>
+                        <div id="message" className="formDiv">
+                            <label id="messageLabel" className="label" htmlFor="message">Message </label>
+                            <textarea id="message" className="inputs" name="message" rows="4" onInput={this.messageHandler}></textarea>
+                        </div>
+                        <p id="warning">{this.state.warningText}</p>
+                        <button id="formButton" onClick={this.submitForm}>Submit</button>
+                    </form>
+                </div>
                 {Footer()}
             </main>
         )
